f1d4a78fb10189a90083aa51e991afa60d6fc820
Tony-ATC <anthony.calascibetta@student.intecbrussel.be>
2021-05-26 18:51:42 +0200

    Section 7 (Index Page) - 7.101  - Classes Added   - index.html   - IndexController

---
+++ src/main/java/udemy/springframework5guru/springrecipe/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package udemy.springframework5guru.springrecipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
